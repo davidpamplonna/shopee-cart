093d4e6ca61fa83f97561e7c97e7af664c9190c6
David Pamplona <davypamp@gmail.com>
2025-08-25 15:40:35 -0300

    implementando a logica do carrinho

---
+++ src/services/cart.js
@@ -0,0 +1,28 @@
+/* Quais ações meu carrinho pode fazer
+CASOS DE USO
+
+1-adicionar
+2-deletar item do carrinho
+3-remover um item
+4- calcular o total
+*/
+
+//adicionar item no carrinho
+async function addItem(userCart){
+
+} 
+
+//deletar item
+async function deleteItem(userCart, name) {
+    
+}
+
+//remover um item
+async function removerItem(userCart, index) {
+    
+}
+
+//calcular total do carrinho
+async function calculateTotal (userCart) {
+    
+}
