277d2bcb88b446e9ae5865afc8e917badc6eb218
David Pamplona <davypamp@gmail.com>
2025-08-25 15:52:31 -0300

    Implementando logica do item

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import createItem from "./services/item.js"
+
+
 const  cart = [];
+
+console.log("Welcome to the your Shopee Cart! ")
+
+const item1 = await createItem("Hotwheels ferrar", 20.99, 1);
+const item2 = await createItem("Hotwheels lamborghini", 39.99, 3)
+
+console.log(item2.subtotal())
